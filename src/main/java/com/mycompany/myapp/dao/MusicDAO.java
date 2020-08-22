b7b9f865b7484129ae4df9cb4dbd7a0d766a5ac4
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2020-08-22 16:53:08 +0900

    등록일시, 아티스트, 제목순으로 정렬가능

--- src/main/java/com/mycompany/myapp/dao/MusicDAO.java
+++ src/main/java/com/mycompany/myapp/dao/MusicDAO.java
@@ -7,6 +7,8 @@ import com.mycompany.myapp.bean.Music;
 public interface MusicDAO {
 	public void insertMusic(Music music);
 	public List<Music> selectMusic();
+	public List<Music> selectMusicByArtist();
+	public List<Music> selectMusicByTitle();
 	public void updateMusic(Music music);
 	public void deleteMusic(int num);
 	public Music selectMusicById(int num);
